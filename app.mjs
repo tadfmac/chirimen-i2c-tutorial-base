ddd2b84a478778a8255b3f050e196c55e50a649b
Hideyuki Kodama <tadfmac@gmail.com>
2025-07-10 11:44:37 +0900

    test

--- app.mjs
+++ app.mjs
@@ -1,3 +1,13 @@
-import {requestI2CAccess} from "chirimen";
+import {requestI2CAccess, SHT40} from "chirimen";
 
 const i2cAccess = await requestI2CAccess();
+
+const i2cPort = i2cAccess.ports.get(1);
+const sht40 = new SHT40(i2cPort, 0x44);
+await sht40.init();
+
+setInterval(()=>{
+  let data = await sht40.readData();
+  console.dir(data);
+},1000);
+
