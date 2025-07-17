b23f1c73de7736a6604639197beb407a9526fac3
Hideyuki Kodama <tadfmac@gmail.com>
2025-07-17 13:02:57 +0900

    test qr

---
+++ appqr.mjs
@@ -0,0 +1,18 @@
+import {requestI2CAccess} from "chirimen";
+import QR from "./qr.js";
+
+const i2cAccess = await requestI2CAccess();
+
+const i2cPort = i2cAccess.ports.get(1);
+const qr = new QR(i2cPort, 0x44);
+await qr.init();
+await qr.setTriggerMode(qr.c.AUTO_SCAN_MODE);
+
+setInterval(async ()=>{
+  if(qr.getDecodeReadyStatus() == 1){
+    let len = await qr.getDecodeLength();
+    let data = await qr.getDecodeData();
+    console.dir(data);
+  }
+},100);
+
