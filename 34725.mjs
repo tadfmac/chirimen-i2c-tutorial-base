9b75502649799c4912f69e4a87aefa85e1bbed5f
Hideyuki Kodama <tadfmac@gmail.com>
2025-07-22 12:07:09 +0900

    add tcs34725

---
+++ 34725.mjs
@@ -0,0 +1,13 @@
+import {requestI2CAccess, TCS34725} from "TCS34725";
+
+const i2cAccess = await requestI2CAccess();
+
+const i2cPort = i2cAccess.ports.get(1);
+const tcs34725 = new TCS34725(i2cPort, 0x29);
+await tcs34725.init();
+
+setInterval(async ()=>{
+  let data = await tcs34725.read();
+  console.dir(data);
+},1000);
+
