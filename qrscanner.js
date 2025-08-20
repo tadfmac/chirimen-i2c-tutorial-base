45d8b64ef912aef55abf19f71867faeefd2b6405
Hideyuki Kodama <tadfmac@gmail.com>
2025-08-20 19:08:30 +0900

    s

---
+++ qrscanner.js
@@ -0,0 +1,84 @@
+// Driver for the QR Code Scanner Unit
+// based from https://github.com/m5stack/M5Unit-QRCode/blob/main/src/M5UnitQRCodeI2C.cpp
+// Programmed by Ryoma Aoki
+
+const qrscanner_QRCODE_ADDR = 0x21;
+const qrscanner_QRCODE_TRIGGER_REG = 0x0000;
+const qrscanner_QRCODE_READY_REG = 0x0010;
+const qrscanner_QRCODE_LENGTH_REG = 0x0020;
+const qrscanner_QRCODE_TRIGGER_MODE_REG = 0x0030;
+const qrscanner_QRCODE_TRIGGER_KEY_REG = 0x0040;
+const qrscanner_QRCODE_DATA_REG = 0x1000;
+const JUMP_TO_BOOTLOADER_REG = 0x00fd;
+const FIRMWARE_VERSION_REG = 0x00fe;
+
+class QRScanner {
+  constructor(i2cPort, slaveAddress) {
+    this.i2cPort = i2cPort;
+    this.i2cSlave = null;
+    this.slaveAddress = slaveAddress;
+  }
+
+  async init() {
+    this.i2cSlave = await this.i2cPort.open(this.slaveAddress);
+  }
+
+  async _write(reg16, data) {
+    let sendData = [];
+    sendData[0] = reg16 & 0x00ff;
+    sendData[1] = (reg16 >> 8) & 0x00ff;
+    const sendArray = sendData.concat(data);
+    return await this.i2cSlave.writeBytes(sendArray);
+  }
+
+  async _read(reg16, length) {
+    let sendData = [];
+    sendData[0] = reg16 & 0x00ff;
+    sendData[1] = (reg16 >> 8) & 0x00ff;
+    await this.i2cSlave.writeBytes(sendData);
+    return await this.i2cSlave.readBytes(length);
+  }
+
+  async setTriggerMode(mode) {
+    await this._write(qrscanner_QRCODE_TRIGGER_MODE_REG, [mode]);
+  }
+  async getTriggerMode() {
+    return await this._read(qrscanner_QRCODE_TRIGGER_MODE_REG, 1)[0];
+  }
+
+  async getDecodeReadyStatus() {
+    return await this._read(qrscanner_QRCODE_READY_REG, 1)[0];
+  }
+
+  async getDecodeLength() {
+    const data = await this._read(qrscanner_QRCODE_LENGTH_REG, 2);
+    return (length = (data[1] << 8) | data[0]);
+  }
+
+  async getDecodeData(length) {
+    const data = await this._read(qrscanner_QRCODE_DATA_REG, length);
+    const decoder = new TextDecoder("utf-8");
+    return decoder.decode(Uint8Array.from(data));
+  }
+
+  wait(ms) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, ms);
+    });
+  }
+
+  async scanData() {
+    for (;;) {
+      if ((await this.getDecodeReadyStatus()) == 1) {
+        const length = await this.getDecodeLength();
+        const data = await this.getDecodeData(length);
+        return data;
+      }
+      await this.wait(10);
+    }
+  }
+}
+
+export default QRScanner;
